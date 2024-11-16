449acffd0527ea74ad90d350e512fe5e5172a512
Pratik-Sanghavi <sanghavipratikr@gmail.com>
2024-11-15 22:18:01 -0600

    Fixes for consumer

--- consumer/consumer.cc
+++ consumer/consumer.cc
@@ -1,24 +1,50 @@
 #include "consumer.h"
 
-Consumer::Consumer(std::string server_address) {
-    std::shared_ptr<Channel> channel = grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials());
-    stub_ = MessageQueue::NewStub(channel);
-}
+#include "message_queue.grpc.pb.h"
+#include <grpcpp/grpcpp.h>
+#include <iostream>
+#include <vector>
+#include <string>
 
-ConsumeMessageResponse Consumer::consumeMessage(int start_offset, int max_messages) {
-    ConsumeRequest request;
-    request.set_start_offset(start_offset);
-    request.set_max_messages(max_messages);
-
-    ConsumeResponse response;
-    ClientContext context;
-
-    Status status = stub_->ConsumeMessage(&context, request, &response);
-
-    if (status.ok()) {
-        return response;
-    } else {
-        std::cout << status.error_code() << ": " << status.error_message() << std::endl;
-        return nullptr;
+class Consumer::Impl {
+public:
+    explicit Impl(std::string server_address) {
+        auto channel = grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials());
+        stub_ = message_queue::message_queue::NewStub(channel);
     }
+
+    std::vector<MessageResponse> ConsumeMessage(int start_offset, int max_messages) {
+        message_queue::ConsumeMessageRequest request;
+        request.set_start_offset(start_offset);
+        request.set_max_messages(max_messages);
+
+        message_queue::ConsumeMessageResponse response;
+        grpc::ClientContext context;
+
+        grpc::Status status = stub_->ConsumeMessage(&context, request, &response);
+
+        if (!status.ok()) {
+            std::cerr << "gRPC error: " << status.error_code() << ": " << status.error_message() << std::endl;
+            return {};
+        }
+        std::vector<MessageResponse> messages;
+        for(const auto& message : response.messages()) {
+            MessageResponse msg;
+            msg.key = message.key();
+            msg.value = message.value();
+            msg.topic = message.topic();
+            msg.timestamp = message.timestamp();
+            messages.push_back(msg);
+        }
+        return messages;
+    }
+
+private:
+    std::unique_ptr<message_queue::message_queue::Stub> stub_;
+};
+
+Consumer::Consumer(std::string server_address) : impl_(std::make_unique<Impl>(server_address)) {}
+
+std::vector<MessageResponse> Consumer::ConsumeMessage(int start_offset, int max_messages) {
+    return impl_->ConsumeMessage(start_offset, max_messages);
 }
--- consumer/consumer.h
+++ consumer/consumer.h
@@ -1,26 +1,25 @@
 #ifndef CONSUMER_H
 #define CONSUMER_H
 
-#include "message_queue.grpc.pb.h"
-#include <grpcpp/grpcpp.h>
+#include <memory>
+#include <string>
+#include <vector>
 
-using grpc::Channel;
-using grpc::ClientContext;
-using grpc::Status;
-using message_queue::MessageQueue;
-using message_queue::Message;
-using message_queue::ConsumeRequest;
-using message_queue::ConsumeResponse;
+struct MessageResponse {
+    std::string key;
+    std::string value;
+    std::string topic;
+    int timestamp;
+};
 
-// Create a consumer class
 class Consumer {
-    private:
-        std::unique_ptr<MessageQueue::Stub> stub_;
-    
-    public:
-        Consumer(std::string server_address);
-        ConsumeMessageResponse consumeMessage(int start_offset, int max_messages);
-}
+private:
+    class Impl;
+    std::unique_ptr<Impl> impl_;
 
+public:
+    explicit Consumer(std::string server_address);
+    std::vector<MessageResponse> ConsumeMessage(int start_offset, int max_messages);
+};
 
 #endif // CONSUMER_H
