8ea4b1fb3b5c2b9238426b0f7e920acf13aeee9a
Pratik-Sanghavi <sanghavipratikr@gmail.com>
2024-12-04 09:10:40 -0600

    Fixed incorrect internal state for consumer groups

--- producer/producer.cc
+++ producer/producer.cc
@@ -58,9 +58,9 @@ public:
                     FlushMessages(broker_ip);
                 }
 
-                // if(broker_timers_.find(broker_ip) == broker_timers_.end()) {
-                //     broker_timers_[broker_ip] = std::thread(&Impl::FlushMessagesPeriodically, this, broker_ip);
-                // }
+                if(broker_timers_.find(broker_ip) == broker_timers_.end()) {
+                    broker_timers_[broker_ip] = std::thread(&Impl::FlushMessagesPeriodically, this, broker_ip);
+                }
             }
             return true;
         } catch (const std::exception& e) {
@@ -98,7 +98,10 @@ private:
                 std::cerr << "Failed to produce messages to broker at: " << broker_ip << std::endl;
             }
 
-            messages.clear();
+            {
+                std::lock_guard<std::mutex> lock(mutex_);
+                message_map_[broker_ip].clear();
+            }
         }
     }
 
